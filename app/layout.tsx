e4774ddd1efe5ec173091b22c19b189f01b106e1
FarazSualeh <24bit44@aiktc.ac.in>
2025-12-12 19:17:02 +0530

    Added Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,8 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        {/* Required for Vercel Speed Insights */}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
-
